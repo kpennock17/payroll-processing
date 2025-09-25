1cd82716b24608c8718a4c7deea70398ffcdedbe
Kevin Pennock <kpennock@usf.edu>
2025-09-24 20:53:31 -0400

    step2

---
+++ cc_5.js
@@ -0,0 +1,28 @@
+let employees = [
+    {name:"Polo",hourlyRate:"Clothes",hoursWorked:"60"},
+    {name:"Polo",hourlyRate:"Clothes",hoursWorked:"60"},
+    {name:"Polo",hourlyRate:"Clothes",hoursWorked:"60"},
+];
+
+function calculateBasePay(20,40)
+
+
+
+
+
+
+const prices = [12,3,12,33];
+const dicounted = prices.map(prices => prices *0.9)
+
+const greeting = {
+    silly:"Hello There",
+    fansy:"Bonjour",
+    
+    function customeGreetings (fansy,silly){
+    return 
+}
+
+
+function calculateShipping(weight) {
+    return weight * 2.5;
+}
